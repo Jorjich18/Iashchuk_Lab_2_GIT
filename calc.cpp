c8b3c923a686df7c10a22d31220cfa616d4e7129
Georgiy <makoson355@gmail.com>
2024-09-26 13:49:16 +0300

    Calculator

---
+++ calc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, op, b;
+	double result;
+	cout << "Input first number: "; cin >> a;
+	cout << "Input number of operation: \n1\t+\n2\t-\n3\t*\n4\t/\n"; cin >> op;
+	cout << "Input second number: "; cin >> b;
+	switch (op)
+	{
+	case 1: result; break;
+	case 2: result; break;
+	case 3: result; break;
+	case 4: result; break;
+	}
+	cout << "Answer: " << result << endl;
+	return 0;
+}
